8b50edc5c29b851e9b07a9ca44c10b7ee70df140
Alex <dn070291dag@gmail.com>
2019-07-29 15:24:53 +0300

    before update

--- Dungeon Escape/Assets/Scripts/MainMenu.cs
+++ Dungeon Escape/Assets/Scripts/MainMenu.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+	void Awake()
+	{
+		Screen.orientation = ScreenOrientation.Landscape;
+	}
+
+
 	public void StartGame()
 	{
 		SceneManager.LoadScene("Game");
